63872d68e4aeef10d9fc1396a840ea445783cc6d
Ashish Raghav <ashishraghav2468@gmail.com>
2024-08-01 17:07:17 +0530

    store Updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import {Outlet} from "react-router-dom";
 import UserContext from './utils/UserContext';
 import { useState } from 'react';
-import Provider from 'react-redux';
+import {Provider} from 'react-redux';
 import appStore from './utils/appStore';
 
 
@@ -12,7 +12,7 @@ const App = () => {
   const [userName,setUserName] = useState("Ashish")
   return (
     <Provider store={appStore}>
-    // used to change the value inside using .Provider
+    {/*used to change the value inside using .Provider */}
     <UserContext.Provider value={{loggedInUser:userName , setUserName}}>  
       <div className="app">
         <Header />
