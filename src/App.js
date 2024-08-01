db4aabb951f2f67dca48dc05ebb67a82d96486bd
Ashish Raghav <ashishraghav2468@gmail.com>
2024-08-01 17:02:31 +0530

    reactStore configured

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from "./components/Header";
 import {Outlet} from "react-router-dom";
 import UserContext from './utils/UserContext';
 import { useState } from 'react';
+import Provider from 'react-redux';
+import appStore from './utils/appStore';
 
 
 
 const App = () => {
   const [userName,setUserName] = useState("Ashish")
   return (
+    <Provider store={appStore}>
     // used to change the value inside using .Provider
     <UserContext.Provider value={{loggedInUser:userName , setUserName}}>  
       <div className="app">
@@ -17,6 +20,7 @@ const App = () => {
         {/* footer */}
       </div>
       </UserContext.Provider>
+      </Provider>
   );
 };
 
