e981f8af4177935bffb477c8ba8a26722d0b144e
Ashish Raghav <ashishraghav2468@gmail.com>
2024-08-07 22:49:51 +0530

    Grouped ContactUs Component TestCases

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,22 +1,27 @@
 import {render,screen} from "@testing-library/react";
 import Contact from "../Contact"
 
-test("Component Should Render",()=>{
-    render(<Contact/>)
-    const element = screen.getByText("Contact Us");
-    expect(element).toBeInTheDocument();
-})
-
-test("Should have Button",()=>{
-    render(<Contact/>)
-    const btn = screen.getByRole("button");
-    expect(btn).toBeInTheDocument();
-})
-
-test("Should load multiple input boxes on the Contact Component",()=>{
-    render(<Contact/>)
-    const inputBoxes = screen.getAllByRole("textbox");    // for checking multiple items
+// describe is used to group multiple test cases , we can also have multiple and nested describe groupings
+describe("Grouping Contact Us Test Cases",()=>{      
+    test("Component Should Render",()=>{
+        render(<Contact/>)
+        const element = screen.getByText("Contact Us");
+        expect(element).toBeInTheDocument();
+    })
     
-    // Assertion
-    expect(inputBoxes.length).toBe(3);
+    test("Should have Button",()=>{
+        render(<Contact/>)
+        const btn = screen.getByRole("button");
+        expect(btn).toBeInTheDocument();
+    })
+    
+    test("Should load multiple input boxes on the Contact Component",()=>{
+        render(<Contact/>)
+    
+        //Querying
+        const inputBoxes = screen.getAllByRole("textbox");    // for checking multiple items
+        
+        // Assertion
+        expect(inputBoxes.length).toBe(3);
+    })
 })
