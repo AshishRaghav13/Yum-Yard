fa71669aec9b13767317e40fc69f33ae3a61fb4f
Ashish Raghav <ashishraghav2468@gmail.com>
2024-08-07 22:36:07 +0530

    Done unit testing for ContactUs Component

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -6,3 +6,17 @@ test("Component Should Render",()=>{
     const element = screen.getByText("Contact Us");
     expect(element).toBeInTheDocument();
 })
+
+test("Should have Button",()=>{
+    render(<Contact/>)
+    const btn = screen.getByRole("button");
+    expect(btn).toBeInTheDocument();
+})
+
+test("Should load multiple input boxes on the Contact Component",()=>{
+    render(<Contact/>)
+    const inputBoxes = screen.getAllByRole("textbox");    // for checking multiple items
+    
+    // Assertion
+    expect(inputBoxes.length).toBe(3);
+})
